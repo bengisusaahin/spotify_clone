5202de29b2966ac6048a730cd29e4cbf6b2851a6
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-02-24 20:51:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spotify_clone
+ App Innovators Mobile Dev Journey Bootcamp Final Project
---
+++ android/app/src/main/kotlin/com/bengisusahin/spotify_clone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bengisusahin.spotify_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
